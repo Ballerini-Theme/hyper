8a9f7ed1834762838f129ac1a680da010f807365
Lucas André <lucasandrefreitas1@gmail.com>
2023-04-08 19:55:08 -0300

    change colors location to execute on npm

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const colors = require("./colors");
-
 module.exports.decorateConfig = (config) => {
   return Object.assign({}, config, {
     cursorColor: colors.cursorColor,
@@ -28,4 +26,17 @@ module.exports.decorateConfig = (config) => {
       lightWhite: colors.foregroundColor,
     },
   });
+};
+
+const colors = {
+  backgroundColor: "#261C1E",
+  cursorColor: "#A45A49",
+  foregroundColor: "#FFF2E7",
+  cursorAccentColor: "#2F2325",
+  selectionColor: "#583c38",
+  borderColor: "#8E646B",
+  black: "#EFC764",
+  green: "#67E480",
+  red: "#E94554",
+  comment: "#8E646B",
 };
